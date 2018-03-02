bc8e6476746d2cc4d49ebbb68e3ae878265abeb1
Composur <xutongxw@163.com>
2018-03-02 11:06:41 +0800

    "to-do-list-one"

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+/**
+ * Created by zhangxinwang on 16/02/2017.
+ */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// alert('success')
+
+class App extends React.Component{//定义组件，继承父类
+    constructor(){//构造函数是和class一起用来创建和初始化对象的特殊方法
+        super()//在封装组件之前会调用react组件的构造函数，当实现React.Component子类的构造函数时，应该在其它任何语句之前调用super()
+        
+    }
+}
