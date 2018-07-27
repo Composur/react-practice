0f9e7155d2e5c41cd5d4f9522ad7ca08e3531c5f
Composur <xutongxw@163.com>
2018-07-27 16:26:54 +0800

    site test

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -1,6 +1,60 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const $ = require('gulp-load-plugins')();
 
-gulp.task('default', function() {
-  // 将你的默认的任务代码放在这
-  
+gulp.task('sass', () => {
+  gulp.src('./public/sass/style.scss')
+    .pipe($.sass({
+      outputStyle: 'compressed'
+    }).on('error', $.sass.logError))
+    .pipe(gulp.dest('./dist/css/'));
+});
+
+gulp.task('js', () => {
+  gulp.src('./public/js/**/*.js')
+    .pipe($.babel({ presets: ['env'] }))
+    .pipe($.concat('main.js'))
+    .pipe($.uglify())
+    .pipe(gulp.dest('./dist/js/'));
+});
+
+gulp.task('img', () => {
+  gulp.src('./public/img/**/*')
+    .pipe($.imagemin())
+    .pipe(gulp.dest('./dist/img/'));
+});
+
+gulp.task('lib', () => {
+  gulp.src('./public/lib/**/*')
+    .pipe(gulp.dest('./dist/lib/'));
+});
+
+gulp.task('font', () => {
+  gulp.src('./public/font/**/*')
+    .pipe(gulp.dest('./dist/font/'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch('./public/sass/**/**.scss', ['sass']);
+  gulp.watch('./public/js/**/*.js', ['js']);
+  // gulp.watch('./routes/index.js', ['js']);
+  gulp.watch('./public/img/**/*', ['img']);
+  gulp.watch('./public/lib/**/*', ['lib']);
+  gulp.watch('./public/font/**/*', ['font']);
+});
+
+gulp.task('build', ['sass', 'js', 'img', 'lib', 'font']);
+
+gulp.task('dev', ['sass', 'js', 'img', 'lib', 'font', 'watch'], function() {
+  $.nodemon({
+    script: './bin/www.js',
+    ignore: ['gulpfile.js', 'node_modules', 'dist', 'public'],
+    ext: 'js'
+  });
+});
+
+gulp.task('default', ['dev']);
+
+gulp.task('clean', () => {
+  gulp.src('./dist')
+    .pipe($.clean());
 });
---
+++ site/public/js/bootstrap.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  /* eslint-disable no-undef */
+  Utils.backTop();
+  Utils.modalSelect();
+  Utils.headerDropMenu();
+  Utils.getSMSCode();
+  Utils.renewCaptcha();
+  Utils.followOrUn();
+  Utils.starTopic();
+  Utils.collectTopic();
+  Utils.deleteReply();
+  Utils.editReply();
+  Utils.upReply();
+  Utils.replyReply();
+});
