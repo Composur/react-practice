52dd8800101a60aa28cf5b9de26046a82119ded4
Composur <xutongxw@163.com>
2018-07-31 18:33:35 +0800

    comments

--- manage/src/components/comment/CommentApp.js
+++ manage/src/components/comment/CommentApp.js
@@ -2,14 +2,23 @@ import React, {Component} from 'react'
 import CommentIput from './CommentIput'
 import CommentList from './CommentList'
 export default class Comment extends Component {
+    constructor(){
+        super()
+        this.state={
+            comments:[]
+        }
+    }
     handleSubmitComment(comment){
-        console.log(comment)
+        this.state.comments.push(comment)
+        this.setState({
+           comments:this.state.comments
+       })
     }
     render(){
         return(
             <div className='wrapper'>
             <CommentIput onSubmit={this.handleSubmitComment.bind(this)}/>
-            <CommentList/>
+            <CommentList comments={this.state.comments}/>
             </div>
         )
     }
--- manage/src/components/comment/CommentList.js
+++ manage/src/components/comment/CommentList.js
@@ -1,18 +1,14 @@
 import React ,{Component} from 'react'
 import Comment from './Comment'
 export default class CommentList extends Component{
+    static defaultProps ={
+        comments:[]
+    }
     render() {
-        const comments = [
-          {username: 'Jerry', content: 'Hello'},
-          {username: 'Tomy', content: 'World'},
-          {username: 'Lucy', content: 'Good'}
-        ]
-    
+        
         return (
             <div>
-                {comments.map((comment,i)=>{
-                    <Comment comment={comment} key={i}/>
-                })}
+                {this.props.comments.map((comment, i) => <Comment comment={comment} key={i} />)}
             </div>
         )
       }
