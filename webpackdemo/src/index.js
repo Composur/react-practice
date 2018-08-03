07c65ed7a0996aea9f34ec353fb5154f9ae9656c
Composur <xutongxw@163.com>
2018-08-03 16:40:08 +0800

    webpack-dev-server

--- webpackdemo/src/index.js
+++ webpackdemo/src/index.js
@@ -1,17 +1,20 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './logo.png';
+import Print from './print'
 function component() {
     var element = document.createElement('div');
-
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello')
-
-    const image=new Image()
-    image.src=Icon
-
-    element.appendChild(image)
+    const btn=document.createElement('button')
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');    
+    btn.innerHTML='Click'
+    btn.onclick=Print
+    element.appendChild(btn)
     return element;
   }
 
   document.body.appendChild(component());
+
+  if(module.hot){
+    module.hot.accept('./print.js',function(){
+      console.log('printme')
+      PrintMe()
+    })
+  }
--- webpackdemo/webpack.config.js
+++ webpackdemo/webpack.config.js
@@ -1,24 +1,40 @@
+const webpack=require('webpack')
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+  entry: {
+   app: './src/index.js',
   },
-  module: {
-    rules: [
+  devtool:'inline-source-map',
+  devServer:{
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,//gzip压缩
+    port: 9000,
+    clientLogLevel: "none",//控制台不答应繁琐信息
+    historyApiFallback: true,//404替换index.html
+    // host: "0.0.0.0",
+    hot: true,
+    noInfo: true
+  },
+  module:{
+    rules:[
       {
-        test: /\.css$/,
+        test:/\.css$/,
         use: ['style-loader', 'css-loader']
-      }, {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader']
-      },
-      {
-        test:/\.(woff|woff2|eot|ttf|otf)$/,
-        use:['file-loader']
       }
     ]
-  }
+  },
+  plugins:[
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title:'Output'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
 };
