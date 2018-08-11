511af0258d17e894957e04ebc81c1baea3c90522
Composur <xutongxw@163.com>
2018-08-11 16:14:59 +0800

    redux

--- react-redux/src/Header.js
+++ react-redux/src/Header.js
@@ -1,31 +1,41 @@
 import React, {Component} from 'react'
 import ProTypes from 'prop-types'
+import {connect} from './connect'
 class Header extends Component {
-    static contextTypes = {
-        store: ProTypes.object
-    }
-    constructor() {
-        super()
-        this.state = {
-            themeColor: ''
-        }
-    }
-    componentWillMount() {
-        const {store}=this.context
-        this._updateThemeColor()
-        store.subscribe(()=>this._updateThemeColor())
-    }
-    _updateThemeColor() {
-        const {store} = this.context
-        const state = store.getState()
-        this.setState({themeColor: state.themeColor})
+    // static contextTypes = {
+    //     store: ProTypes.object
+    // }
+    // constructor() {
+    //     super()
+    //     this.state = {
+    //         themeColor: ''
+    //     }
+    // }
+    // componentWillMount() {
+    //     const {store}=this.context
+    //     this._updateThemeColor()
+    //     store.subscribe(()=>this._updateThemeColor())
+    // }
+    // _updateThemeColor() {
+    //     const {store} = this.context
+    //     const state = store.getState()
+    //     this.setState({themeColor: state.themeColor})
+    // }
+    static propsTypes={
+        themeColor:ProTypes.string
     }
     render() {
         return (
             <div>
-                <h1 style={{color:this.state.themeColor}}>Header</h1>
+                <h1 style={{color:this.props.themeColor}}>Header</h1>
             </div>
         )
     }
 }
+const mapStateToProps=(state)=>{
+    return{
+        themeColor:state.themeColor
+    }
+}
+Header=connect(mapStateToProps)(Header)
 export default Header
--- react-redux/src/connect.js
+++ react-redux/src/connect.js
@@ -1,32 +1,37 @@
-import React, {Component} from 'react'
+import React ,{Component} from 'react'
 import PropTypes from 'prop-types'
-
-const mapStateToProps=(state)=>{
-    return{
-        themeColor:state.themeColor,
-        themeName:state.themeName,
-        fullName:`${state.firstName}${state.lastName}`
-    }
-}
-
-export const  connect = (mapStateToProps) => {
+export const connect = (mapStateToProps) => (WrappedComponent) => {
     class Connect extends Component {
-        static contextTypes = {
-            store: PropTypes.object
-        }
-        constructor() {
-            super()
-            this.state = {}
-        }
-        render() {
-            const {store}=this.context
-            let stateProps=mapStateToProps(store.getState())
-            return (
-                <div>
-                    <WrappedComponent {...stateProps}/>
-                </div>
-            )
-        }
+      static contextTypes = {
+        store: PropTypes.object
+      }
+  
+      constructor () {
+        super()
+        this.state = { allProps: {} }
+      }
+  
+      componentWillMount () {
+        const { store } = this.context
+        this._updateProps()
+        store.subscribe(() => this._updateProps())
+      }
+  
+      _updateProps () {
+        const { store } = this.context
+        let stateProps = mapStateToProps(store.getState(), this.props) // 额外传入 props，让获取数据更加灵活方便
+        this.setState({
+          allProps: { // 整合普通的 props 和从 state 生成的 props
+            ...stateProps,
+            ...this.props
+          }
+        })
+      }
+  
+      render () {
+        return <WrappedComponent {...this.state.allProps} />
+      }
     }
+  
     return Connect
-}
+  }
