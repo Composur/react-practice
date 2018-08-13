a9405ef67ee36719c0f8c4ffcf030eab2d849539
Composur <xutongxw@163.com>
2018-08-13 12:11:00 +0800

    test

--- react-redux/src/Content.js
+++ react-redux/src/Content.js
@@ -1,32 +1,34 @@
-import React ,{Component} from 'react'
-import ProTypes from 'prop-types'
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import ThemeSwitch from './ThemeSwitch'
-export default class Content extends Component{
-    static contextTypes={
-        store:ProTypes.object
+import {connect} from './connect'
+import {conn} from './react-redux'
+ class Content extends Component {
+    static propTypes = {
+        themeColor: PropTypes.string
     }
-    constructor(){
-        super()
-        this.state={
-            themeColor:''
-        }
-    }
-    componentWillMount(){
-        const {store}=this.context
-        this._updateThemeColor()
-        store.subscribe(()=>this._updateThemeColor())
-    }
-    _updateThemeColor(){
-        const {store}=this.context
-        const state=store.getState()
-        this.setState({themeColor:state.themeColor})
-    }
-    render(){
-        return(
+
+    // static contextTypes={     store:ProTypes.object } constructor(){     super()
+    //    this.state={         themeColor:''     } } componentWillMount(){     const
+    // {store}=this.context     this._updateThemeColor()
+    // store.subscribe(()=>this._updateThemeColor()) } _updateThemeColor(){
+    // const {store}=this.context     const state=store.getState()
+    // this.setState({themeColor:state.themeColor}) }
+
+    render() {
+        return (
             <div>
-                <p style={{color:this.state.themeColor}}>content</p>
+                <p style={{
+                    color: this.props.themeColor
+                }}>content</p>
                 <ThemeSwitch/>
             </div>
         )
     }
 }
+const mapStateToProps = (state) => {
+    return {themeColor: state.themeColor}
+}
+Content = connect(mapStateToProps)(Content)
+
+export default Content
