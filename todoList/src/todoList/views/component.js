bbbddcb078d89f9f5dd2782ea3d3a1277f997613
composure <xutong1107@gmail.com>
2019-08-01 22:07:44 +0800

    init

---
+++ todoList/src/todoList/views/component.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class Todos extends Component {
+  constructor(){
+    super()
+    this.state={
+
+    }
+  }
+  render() {
+    return (
+      <div>
+        <input/> <button>增加</button>
+      </div>
+    )
+  }
+}
+
+export default Todos
---
+++ todoList/src/todoList/views/container.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {connect} from 'react-redux'
+import reducer from '../reducer'
+import actions from '../actons'
+connect(state=>({
+
+}),{})
