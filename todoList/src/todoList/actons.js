35ba380c8548946a9102b16a5111bffd4a091bda
composure <xutong1107@gmail.com>
2019-08-02 09:37:10 +0800

    feat(todoList):add actionType

---
+++ todoList/src/todoList/actons.js
@@ -0,0 +1,20 @@
+import {ADD_TODO,TOGGLE_TODO,REMOVE_TODO} from './actionTypes'
+
+let nextTodoId=0
+
+export const addTodo=(text)=>({
+  type:ADD_TODO,
+  id:nextTodoId++,//每增加一项id加一
+  text:text,
+  complete:false
+})
+
+export const toggleTodo=(id)=>({
+  type:TOGGLE_TODO,
+  id:id
+})
+
+export const removeTodo=(id)=>({
+  type:REMOVE_TODO,
+  id:id
+})
