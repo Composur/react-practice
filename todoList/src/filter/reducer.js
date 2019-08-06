f1bb17bf1fb2a92e535f4ca74d7bc61222a51f51
composure <xutong1107@gmail.com>
2019-08-06 11:58:28 +0800

    feat(todo):actionType

---
+++ todoList/src/filter/reducer.js
@@ -0,0 +1,11 @@
+import {SET_FILTER} from './actionTypes'
+import {FilterTypes} from '../../src/constants'
+
+export default (state=FilterTypes.ALL,action)=>{
+  switch(action.type){
+    case SET_FILTER:
+    return action.filter
+    default:
+    return state
+  }
+}
