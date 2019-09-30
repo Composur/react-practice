8106f2176b3e4daa836ad3374e4d3b76a044ad0b
composure <xutong1107@gmail.com>
2019-09-30 11:38:28 +0800

    test

--- react-chat/client/src/components/logo/logo.js
+++ react-chat/client/src/components/logo/logo.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import logo from './image/haizhi.png'
+import logo from './image/WechatIMG35568.png'
 import './logo.less'
 
 export default function (){
     return(
         <div className='logo-container'>
-            <img src='https://i.loli.net/2019/09/17/RIbSJdf3wyDM7W5.png' alt='logo' className='logo'></img>
+            <div className='logo'>WeChat</div>
         </div>
     )
 }
